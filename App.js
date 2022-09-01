0886ddbb1abe58e9e677a747e5060dee1f74740f
Oğuzhan Aydın <ogzhnaydnyazilim@gmail.com>
2022-09-01 04:56:12 +0300

    Demo App

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Dimensions } from "react-native"
+import GridLayout from "@ogzhnaydn/react-native-grid-layout"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <GridLayout
+        numCols={3}
+        numRows={3}
+        bodyStyle={{ backgroundColor: "#1e232a", flex: 1 }}
+      >
+        <View
+          colStart={1}
+          colEnd={2}
+          rowStart={1}
+          rowEnd={1}
+          style={{ backgroundColor: "red", flex: 1 }}
+        ></View>
+        <View
+          colStart={3}
+          colEnd={3}
+          rowStart={1}
+          rowEnd={1}
+          style={{ backgroundColor: "blue", flex: 1 }}
+        ></View>
+        <View
+          colStart={1}
+          colEnd={2}
+          rowStart={2}
+          rowEnd={3}
+          style={{ backgroundColor: "black", flex: 1 }}
+        ></View>
+        <View
+          colStart={3}
+          colEnd={3}
+          rowStart={2}
+          rowEnd={3}
+          style={{ backgroundColor: "white", flex: 1 }}
+        ></View>
+      </GridLayout>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
